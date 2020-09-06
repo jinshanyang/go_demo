3f4f0e2d719a2e862bfbe096fbb196831fbba9c4
yangjinshan.shine <yangjinshan.shine@bytedance.com>
2020-09-06 11:27:28 +0800

    add

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/jinshanyang/go_demo
 
-go 1.13
+go 1.14
--- test/go_test.go
+++ test/go_test.go
@@ -4,8 +4,12 @@ import (
 	"errors"
 	"fmt"
 	"testing"
+	"time"
 )
 
+/*
+* 字符是放常量池, 对象比较的是地址
+*/
 func TestError(t *testing.T) {
 	err1 := errors.New("test")
 	err2 := errors.New("test")
@@ -14,7 +18,7 @@ func TestError(t *testing.T) {
 	}else {
 		fmt.Print("not equal")
 	}
-
+    fmt.Println("")
 	s1 := "test"
 	s2 := "test"
 	if s1 == s2 {
@@ -22,4 +26,35 @@ func TestError(t *testing.T) {
 	}else {
 		fmt.Print("not equal")
 	}
+	fmt.Println("")
+}
+
+func TestTime(t2 *testing.T) {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+	fmt.Print(testReturn())
+}
+
+func testReturn()(res string){
+	res = "hello"
+	return "world"
 }
